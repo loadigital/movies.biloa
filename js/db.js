60987d8b2c8c25f960da0e64312ee6b7be252b75
loadigital <95947050+loadigital@users.noreply.github.com>
2023-05-05 15:47:51 -0500

    Update db.js

--- js/db.js
+++ js/db.js
@@ -2,14 +2,19 @@
 
 const  db =[
 
-    {
-        id:"A000",
-        title:"hola",
+  {
+        id:"A458",
+        id_POST:"502356",
+        title:"Súper Mario Bros. La película",
         genero:"Acción",
+        genres:"Animación, Aventura, Familia, Fantasía, Comedia",
         tipo:"Pelicula",
-        año:"",
-        imgLink:"https://www.themoviedb.org/t/p/w220_and_h330_face/zSw2JeQ03GivcS4VKJmWK5sYi1F.jpg",
-        posterLink:"https://www.themoviedb.org/t/p/w220_and_h330_face/zSw2JeQ03GivcS4VKJmWK5sYi1F.jpg",
+        año:"2023",
+        imgLink:"https://www.themoviedb.org/t/p/w300_and_h450_bestv2/zNKs1T0VZuJiVuhuL5GSCNkGdxf.jpg",
+        posterLink:"https://infobiloa.github.io/BD.BILOA.COM/posterPeliculas/A458 Súper Mario Bros.html",
+        LINK_MOVIES:"https://infobiloa.github.io/v2.zplayer/fluid.html?id=eyJpZCI6WyIxanpPb1ppTjRlOEtRYVUtTm1VUTdWSVYxMlZ3UldHR0MiXX0=",
+        LINK_DESCARGA:"https://drive.google.com/u/2/uc?id=1jzOoZiN4e8KQaU-NmUQ7VIV12VwRWGGC&export=download",
+        TRAILER:"https://www.youtube.com/embed/SvJwEiy2Wok",
     
     },
         
