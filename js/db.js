9d24b94ff9e2b8c206189bd149932fe98e2846b7
loadigital <95947050+loadigital@users.noreply.github.com>
2023-05-15 21:24:48 -0500

    Update db.js

--- js/db.js
+++ js/db.js
@@ -9,6 +9,21 @@
 const  db =[
 
     {
+        id:"A467",
+        id_POST:"552688",
+        title:"La Madre",
+        genero:"Acción",
+        genres:"Acción, Suspense",
+        tipo:"Pelicula",
+        año:"2023",
+        imgLink:"https://www.themoviedb.org/t/p/w300_and_h450_bestv2/pNYtkngatCVyDvbSkaYV2ZOr0lS.jpg",
+        posterLink:"https://infobiloa.github.io/BD.BILOA.COM/posterPeliculas/A467 La Madre.html",
+        LINK_MOVIES:"http://moviesbiloa.byethost16.com/stream/720/iZF1mUC0N16wZ8O/__001",
+        LINK_DESCARGA:"https://drive.google.com/u/9/uc?id=1dOMntpqAoYtqP4EYgvWjTWr6b_GgkRFK&export=download",
+        TRAILER:"https://www.youtube.com/embed/Awmni5mbyWE",
+    
+    },
+    {
         id:"A465",
         id_POST:"763148",
         title:"Se terminó el tiempo",
