0aa3a9b31aed1b841ec9cf3d9f9f6483c29a538c
loadigital <95947050+loadigital@users.noreply.github.com>
2023-05-23 22:28:39 -0500

    Update db.js

--- js/db.js
+++ js/db.js
@@ -18,7 +18,7 @@ const  db =[
         año:"2023",
         imgLink:"https://www.themoviedb.org/t/p/w300_and_h450_bestv2/pNYtkngatCVyDvbSkaYV2ZOr0lS.jpg",
         posterLink:"../plantillas/posterPeliculas/A467 la madre.html",
-        LINK_MOVIES:"http://moviesbiloa.byethost16.com/v/iZF1mUC0N16wZ8O",
+        LINK_MOVIES:"http://moviesbiloa.byethost16.com/stream/720/iZF1mUC0N16wZ8O/__001",
         LINK_DESCARGA:"https://drive.google.com/u/9/uc?id=1dOMntpqAoYtqP4EYgvWjTWr6b_GgkRFK&export=download",
         TRAILER:"https://www.youtube.com/embed/Awmni5mbyWE",
     
