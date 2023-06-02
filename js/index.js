0d87a801c2970186d6a5b9e5f8973597d60813de
loadigital <95947050+loadigital@users.noreply.github.com>
2023-06-01 21:36:03 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -8,9 +8,16 @@ menu.innerHTML = `
         <div class="content-header" id="content-header">
             <div class="co-icon-1">
                 <div class="icon-menu icons-style"><i class="bi-list"></i></div>
+                <div class="co-logo-2"><img src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgIcl_Aw2611bC9UWhKHC7O1PKnvUWR3B6oATFG0b4iAVF1S9Klv-FMLde3DQno56WXIINX1BVejaFxfCwucf3eduX80ARWh3QREsHuQ6Npql4MiKCvnX4eVwU5P7rV6oXWx0N74VE_0_LwLDOTZ_HbvsQgDHbLU0blEc5Hu7_7GrbBaN0jUS2f7I2g/s166/LOGO1.png"></div>
             </div>
             <div class="co-icon-2">
                 <div class="co-text"><p>Películas</p></div>
+                <div class="bc1">
+            
+                <div class="bc-1"><a href="#"><i class="bi-collection-play-fill icon"></i>Películas</a></div>
+                <div class="bc-1"><a href="page/series.html"><i class="bi-tv-fill icon"></i>Series</a></div>
+                <div class="bc-1"><a href="#"><i class="bi-info-circle-fill icon"></i>Información</a></div>
+            </div>
             </div>
             
             <div class="co-icon-3">
@@ -26,16 +33,16 @@ menu.innerHTML = `
 subMenu.className="content-submenu"
 subMenu.innerHTML = `
     <div class="co-submenu">
-        <li class="link act_recientes"><a href="https://loadigital.github.io/movies.biloa/index.html">Recientes</a></li>
-        <li class="link act_accion" ><a href="https://loadigital.github.io/movies.biloa/page/pagePeliculas/accion.html">Acción</a></li>
-        <li class="link act_animacion" ><a href="https://loadigital.github.io/movies.biloa/page/pagePeliculas/animacion.html">Animación</a></li>
-        <li class="link act_aventura" ><a href="https://loadigital.github.io/movies.biloa/page/pagePeliculas/aventura.html">Aventura</a></li>
-        <li class="link act_belicos" ><a href="https://loadigital.github.io/movies.biloa/page/pagePeliculas/belicos.html">Bélicos</a></li>
-        <li class="link act_comedia" ><a href="https://loadigital.github.io/movies.biloa/page/pagePeliculas/comedia.html">Comedia</a></li>
-        <li class="link act_documental" ><a href="https://loadigital.github.io/movies.biloa/page/pagePeliculas/documental.html">Documental</a></li>
-        <li class="link act_historico" ><a href="https://loadigital.github.io/movies.biloa/page/pagePeliculas/historico.html">Histórico</a></li>
-        <li class="link act_romance" ><a href="https://loadigital.github.io/movies.biloa/page/pagePeliculas/romance.html">Romance</a></li>
-        <li class="link act_terror" ><a href="https://loadigital.github.io/movies.biloa/page/pagePeliculas/terror.html">Terror</a></li>
+    <li class="link act_recientes"><a href="https://loadigital.github.io/movies.biloa/index.html">Recientes</a></li>
+    <li class="link act_accion" ><a href="https://loadigital.github.io/movies.biloa/page/pagePeliculas/accion.html">Acción</a></li>
+    <li class="link act_animacion" ><a href="https://loadigital.github.io/movies.biloa/page/pagePeliculas/animacion.html">Animación</a></li>
+    <li class="link act_aventura" ><a href="https://loadigital.github.io/movies.biloa/page/pagePeliculas/aventura.html">Aventura</a></li>
+    <li class="link act_belicos" ><a href="https://loadigital.github.io/movies.biloa/page/pagePeliculas/belicos.html">Bélicos</a></li>
+    <li class="link act_comedia" ><a href="https://loadigital.github.io/movies.biloa/page/pagePeliculas/comedia.html">Comedia</a></li>
+    <li class="link act_documental" ><a href="https://loadigital.github.io/movies.biloa/page/pagePeliculas/documental.html">Documental</a></li>
+    <li class="link act_historico" ><a href="https://loadigital.github.io/movies.biloa/page/pagePeliculas/historico.html">Histórico</a></li>
+    <li class="link act_romance" ><a href="https://loadigital.github.io/movies.biloa/page/pagePeliculas/romance.html">Romance</a></li>
+    <li class="link act_terror" ><a href="https://loadigital.github.io/movies.biloa/page/pagePeliculas/terror.html">Terror</a></li>
     </div>
        
 
@@ -58,9 +65,8 @@ co_menu_lateral.innerHTML = `
             </div>
             <div class="bc">
             
-                <div class="bc-1"><a href="https://loadigital.github.io/movies.biloa/index.html"><i class="bi-collection-play-fill icon"></i>Películas</a></div>
-               <!-- <div class="bc-1"><a href="https://loadigital.github.io/movies.biloa/page/series.html"><i class="bi-tv-fill icon"></i>Series</a></div>-->
-               <div class="bc-1"><a href="go:B000"><i class="bi-tv-fill icon"></i>Series</a></div>
+                <div class="bc-1"><a href="#"><i class="bi-collection-play-fill icon"></i>Películas</a></div>
+                <div class="bc-1"><a href="page/series.html"><i class="bi-tv-fill icon"></i>Series</a></div>
                 <div class="bc-1"><a href="#"><i class="bi-info-circle-fill icon"></i>Información</a></div>
             </div>
 
@@ -74,7 +80,7 @@ bX.addEventListener("click", ()=>{document.querySelector(".co-menu-lateral").sty
 
 const bSearch = document.querySelector(".bi-search");
 bSearch.addEventListener("click", function(){
-    window.open('https://loadigital.github.io/movies.biloa/page/search.html' , "_self");
+    window.open('page/search.html' , "_self");
 })
 
 
