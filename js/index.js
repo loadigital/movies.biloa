c56aebc6b70cfa6de41b04c9d3d151ee79b658a3
loadigital <95947050+loadigital@users.noreply.github.com>
2023-06-12 22:10:49 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -18,6 +18,7 @@ menu.innerHTML = `
                <!-- <div class="bc-1"><a href="page/series.html"><i class="bi-tv-fill icon"></i>Series</a></div>-->
                 <div class="bc-1"><a href="https://loadigital.github.io/movies.biloa/series B000.html"><i class="bi-tv-fill icon"></i>Series</a></div>
                 <div class="bc-1"><a href="#"><i class="bi-info-circle-fill icon"></i>Información</a></div>
+                <div class="bc-1"><a href="https://loadigital.github.io/descargar-Biloa/"><i class="bi-android2 icon"></i>Descargar App</a></div>
             </div>
             </div>
             
@@ -72,7 +73,8 @@ co_menu_lateral.innerHTML = `
                 <div class="bc-1"><a href="https://loadigital.github.io/movies.biloa/index.html"><i class="bi-collection-play-fill icon"></i>Películas</a></div>
                 <div class="bc-1"><a href="https://loadigital.github.io/movies.biloa/series B000.html"><i class="bi-tv-fill icon"></i>Series</a></div>
                 <div class="bc-1"><a href="#"><i class="bi-info-circle-fill icon"></i>Información</a></div>
-                <!--<div class="bc-1"><a target="_blank" href="https://loadigital.github.io/movies.biloa/"><i class="bi-globe icon"></i>Versión Web</a></div>-->
+                <div class="bc-1"><a href="https://loadigital.github.io/descargar-Biloa/"><i class="bi-android2 icon"></i>Descargar App</a></div>
+                
             </div>
 
 `
