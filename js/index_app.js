49bbd317a2213a4a262364e98d825b29b00bc86b
loadigital <95947050+loadigital@users.noreply.github.com>
2023-06-19 16:20:58 -0500

    Update index_app.js

--- js/index_app.js
+++ js/index_app.js
@@ -90,7 +90,7 @@ bX.addEventListener("click", ()=>{document.querySelector(".co-menu-lateral").sty
 
 const bSearch = document.querySelector(".bi-search");
 bSearch.addEventListener("click", function(){
-    window.open('https://loadigital.github.io/movies.biloa/page/search.html' , "_self");
+    window.open('https://loadigital.github.io/movies.biloa/page/search_app.html' , "_self");
 });
 
 const bFilter= document.querySelector(".bi-filter");
