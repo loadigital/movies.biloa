c4e4639bb3754ae3afc963f41da56927f6d8ca8c
loadigital <95947050+loadigital@users.noreply.github.com>
2024-04-08 13:21:39 -0500

    Add files via upload

---
+++ pege&rolda&/rolda&cs=$uno%unoid=.js
@@ -0,0 +1,111 @@
+//Nell recargada
+const S014 = {
+    "temporadas":"1",
+    "temporada1":[
+                "https://mo.biloa.website/biloa/QNwTkDFfZR7orrv",//T1-E1
+                "https://mo.biloa.website/biloa/8PXhWpoGdwSY5dQ",//T1-E2
+                "https://mo.biloa.website/biloa/Uroa1MNKUPdMiah",//T1-E3
+                "https://mo.biloa.website/biloa/xLcdYzBsifWPOl8",//T1-E4
+                "https://mo.biloa.website/biloa/7DlBPm01fhiwp9l",//T1-E5
+                "https://mo.biloa.website/biloa/85PsohxfeOoGH7K",//T1-E6
+                "https://mo.biloa.website/biloa/Z49hsaE23yESROR",//T1-E7
+                "https://mo.biloa.website/biloa/UxM9rWgp6su6TTn",//T1-E8 
+                ]  
+}
+//Los anillos de poder
+const S013 = {
+    "temporadas":"1",
+    "temporada1":[
+                "https://mo.biloa.website/biloa/5xsbyEq8vZx1wun",//T1-E1
+                "https://mo.biloa.website/biloa/n39wpqz9PgppFam",//T1-E2
+                "https://mo.biloa.website/biloa/u0RWE5fV6kbXhBp",//T1-E3
+                "https://mo.biloa.website/biloa/cOEYnZDkXoz6VEk",//T1-E4
+                "https://mo.biloa.website/biloa/oKbvqPhAssVqWeG",//T1-E5
+                "https://mo.biloa.website/biloa/z1xc291HnjeKWAs",//T1-E6
+                "https://mo.biloa.website/biloa/t3FOtH3Bd6ViRFO",//T1-E7
+                "https://mo.biloa.website/biloa/jCkpXSTdxCZ1dz2",//T1-E8 
+                ]  
+}
+//La lista terminal
+const S012 = {
+    "temporadas":"1",
+    "temporada1":[
+                "https://mo.biloa.website/biloa/BT9PFKe8U4Llf9r",//T1-E1
+                "https://mo.biloa.website/biloa/EyIvz5tpV6kFQEV",//T1-E2
+                "https://mo.biloa.website/biloa/b0yuJXfTrRWYemS",//T1-E3
+                "https://mo.biloa.website/biloa/GsUOft829vTCTtP",//T1-E4
+                "https://mo.biloa.website/biloa/DOEB3vbOHNsakfi",//T1-E5
+                "https://mo.biloa.website/biloa/3LCUJSgkaA1SxSE",//T1-E6
+                "https://mo.biloa.website/biloa/qFx27uBVK5Ih3O8",//T1-E7
+                "https://mo.biloa.website/biloa/4JmXPLD9cZ86u5M",//T1-E8 
+                ]  
+}
+//Halo
+const S011 = {
+    "temporadas":"2",
+    "temporada1":[
+                    "https://mo.biloa.website/biloa/jUCi4uPrvPFwVxN",//T1-E1
+                    "https://mo.biloa.website/biloa/JDwuMPhICockTFh",//T1-E2
+                    "https://mo.biloa.website/biloa/B2UIsviwUMhL6kz",//T1-E3
+                    "https://mo.biloa.website/biloa/HjMRhFHgP4plAeB",//T1-E4
+                    "https://mo.biloa.website/biloa/iQaWFsAZNUxcfBa",//T1-E5
+                    "https://mo.biloa.website/biloa/C5Q6deOrvzxeijg",//T1-E6
+                    "https://mo.biloa.website/biloa/fIRVLKwqc84XHM6",//T1-E7
+                    "https://mo.biloa.website/biloa/p746Rq5xH4T5dn7",//T1-E8
+                    "https://mo.biloa.website/biloa/adOhcdd1MzclvVW",//T1-E9
+                 ],
+     "temporada2":[
+                    "https://mo.biloa.website/biloa/G9OxnJgNfJE9xZb",//T2-E1
+                    "https://mo.biloa.website/biloa/8dUBtfHPNJXcKXp",//T2-E2
+                    "https://mo.biloa.website/biloa/K6E8emFRo0qNOhG",//T2-E3
+                    "https://mo.biloa.website/biloa/KIDlzVnKuk9b7ZQ",//T2-E4
+                    "https://mo.biloa.website/biloa/9GmJSu7MsHAy71c",//T2-E5
+                    "https://mo.biloa.website/biloa/5TuMyqidQ8byPSb",//T2-E6
+                    "https://mo.biloa.website/biloa/gvaC7PznIlQ0UpM",//T2-E7
+                    "https://mo.biloa.website/biloa/TNUL9E0WhcoJGAs",//T2-E8
+                  ] 
+                     
+    
+}
+//Supersex
+const S010 = {
+    "temporadas":"1",
+    "temporada1":[
+                    "https://mo.biloa.website/biloa/Div0IXin7IOeuZM",//T1-E1
+                    "https://mo.biloa.website/biloa/2m6x6voYWZRoRNX",//T1-E2
+                    "https://mo.biloa.website/biloa/6CBsaQydYkyuQSH",//T1-E3
+                    "https://mo.biloa.website/biloa/ge3YbEzNiQQsWEq",//T1-E4
+                    "https://mo.biloa.website/biloa/RvLcUKkkBE3NdiI",//T1-E5
+                    "https://mo.biloa.website/biloa/KSmogtljvqexw2e",//T1-E6
+                    "https://mo.biloa.website/biloa/TDEe2X78E7PX79E",//T1-E7
+                ]
+       
+}
+//Bandidos
+const S009 = {
+    "temporadas":"1",
+    "temporada1":[
+                "https://mo.biloa.website/biloa/GlNwOn8j8Isn02G",//T1-E1
+                "https://mo.biloa.website/biloa/aLZkyhRvtbVQAYu",//T1-E2
+                "https://mo.biloa.website/biloa/51ulzrS1J34prah",//T1-E3
+                "https://mo.biloa.website/biloa/7UvdTauv9T2hRrl",//T1-E4
+                "https://mo.biloa.website/biloa/jtIVkCLzWvUA571",//T1-E5
+                "https://mo.biloa.website/biloa/MkjNbMjAqnujLtL",//T1-E6
+                "https://mo.biloa.website/biloa/GyaAbClhQTvvwiQ",//T1-E7
+                ]
+}
+
+//El problema de los 3 cuerpos
+const S008 = {
+    "temporadas":"1",
+    "temporada1":[
+                "https://mo.biloa.website/biloa/3bQeFRFG5hgSRBo",//T1-E1
+                "https://mo.biloa.website/biloa/RM2MdPdov5CMdiE",//T1-E2
+                "https://mo.biloa.website/biloa/LzcMgOdC6IiyYHN",//T1-E3
+                "https://mo.biloa.website/biloa/62gFJBZa7O9gRNe",//T1-E4
+                "https://mo.biloa.website/biloa/CPMiTxmG2bmVY6i",//T1-E5
+                "https://mo.biloa.website/biloa/GQEI9YNtTmLYLKh",//T1-E6
+                "https://mo.biloa.website/biloa/AcNbS9xRB0FrSLl",//T1-E7
+                "https://mo.biloa.website/biloa/AyYW5KZOHGVqsJP"//T1-E8
+                ]   
+}
