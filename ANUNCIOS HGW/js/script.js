4df90cedbc8c32cd3f8507781295e86a9b27a6ab
loadigital <95947050+loadigital@users.noreply.github.com>
2024-07-26 20:00:53 -0500

    Update script.js

--- ANUNCIOS HGW/js/script.js
+++ ANUNCIOS HGW/js/script.js
@@ -21,14 +21,15 @@ const productos = data => {
         
         insert.innerHTML += `
         <div class="card">
-            <div class="card-img col-12 flexrow-c">
-                 <a href=""></a><img class="img-card" src="${content.img}" alt="">
-            </div>
+           
             <div class="card-body">
                 <h2 class="title-card">${content.title}</h2>
                 <p class="flexrow-c">Envío gratis a todo el país "Colombia"...</p>
                 <a  target="_blank" href="https://wa.me/573175284010?text=Hola%20deseo%20mas%20información%20del%20siguiente%20producto  *${content.title}*" class="col-12 flexrow-c"><button class="bot-2 bi-whatsapp"> Mas Información</button></a>
                 <p class="desc col-8 flexrow-c">${content.descripcion}</p>
+            </div>
+             <div class="card-img col-12 flexrow-c">
+                 <a href=""></a><img class="img-card" src="${content.img}" alt="">
             </div>
         </div> 
         `
